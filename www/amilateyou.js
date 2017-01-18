8e52db35db2ff55178eb2cfc30167f458858b2dd
vishuhanda001 <vishuhanda001@gmail.com>
2017-01-18 23:15:43 +0530

    first commit

---
+++ www/amilateyou.js
@@ -0,0 +1,9 @@
+var exec = require('cordova/exec');
+
+// exports.coolMethod = function(arg0, success, error) {
+//     exec(success, error, "amilateyou", "coolMethod", [arg0]);
+// };
+
+exports.myMethod = function(arg0, success, error) {
+    exec(success, error, "amilateyou", "myMethod", [arg0]);
+};
